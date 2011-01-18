e52251ed015c1d7888a74d3d02b4990da26ebdce
ubuntu on my asus <joshua.calloway@gmail.com>
2011-01-17 23:40:33 -0600

    added TabActivity for tabs

--- src/com/papp/NextMatch.java
+++ src/com/papp/NextMatch.java
@@ -21,52 +21,46 @@ import android.widget.Gallery.LayoutParams;
 
 
 public class NextMatch extends Activity {
-	private final static String TAG="NextMatch";
-	  private void installDatabaseIfNeeded() {
-		    InstallDatabase installDb = new InstallDatabase(this);
-		    installDb.createDataBase();
-		  }
-	/** Called when the activity is first created. */
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.installDatabaseIfNeeded();
-		setTitle(R.string.version);
-		setContentView(R.layout.nextmatch);
-    ImageView clubPic = (ImageView)findViewById(R.id.club_imgview);
-		Log.v(TAG, "clubPic : " + clubPic);
-		Bitmap resized = resizeImage( R.drawable.valleylo_club);
-		BitmapDrawable bmd = new BitmapDrawable(resized);
-		clubPic.setImageDrawable(bmd);
-		clubPic.setScaleType(ScaleType.FIT_XY);
-	}
+    private final static String TAG="NextMatch";
 
-	private Bitmap resizeImage(int drawableId){
-      
-      // load the origial BitMap (500 x 500 px)
-      Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), 
-             drawableId);
-      
-      int width = bitmapOrg.getWidth();
-      int height = bitmapOrg.getHeight();
-      int newWidth = 100;
-      int newHeight = 100;
-      
-      // calculate the scale - in this case = 0.4f
-      float scaleWidth = ((float) newWidth) / width;
-      float scaleHeight = ((float) newHeight) / height;
-      
-      // createa matrix for the manipulation
-      Matrix matrix = new Matrix();
-      // resize the bit map
-      matrix.postScale(scaleWidth, scaleHeight);
-      // rotate the Bitmap
-      matrix.postRotate(0);
+    /** Called when the activity is first created. */
+    @Override public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setTitle(R.string.version);
+	setContentView(R.layout.nextmatch);
+	ImageView clubPic = (ImageView)findViewById(R.id.club_imgview);
+	Log.v(TAG, "clubPic : " + clubPic);
+	Bitmap resized = resizeImage( R.drawable.valleylo_club);
+	BitmapDrawable bmd = new BitmapDrawable(resized);
+	clubPic.setImageDrawable(bmd);
+	clubPic.setScaleType(ScaleType.FIT_XY);
+    }
 
-      // recreate the new Bitmap
-      Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, 
-                        width, height, matrix, true); 
-      return resizedBitmap;
-  }
-
+    private Bitmap resizeImage(int drawableId){
+	
+	// load the origial BitMap (500 x 500 px)
+	Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), 
+							drawableId);
+	
+	int width = bitmapOrg.getWidth();
+	int height = bitmapOrg.getHeight();
+	int newWidth = 100;
+	int newHeight = 100;
+	
+	// calculate the scale - in this case = 0.4f
+	float scaleWidth = ((float) newWidth) / width;
+	float scaleHeight = ((float) newHeight) / height;
+	
+	// createa matrix for the manipulation
+	Matrix matrix = new Matrix();
+	// resize the bit map
+	matrix.postScale(scaleWidth, scaleHeight);
+	// rotate the Bitmap
+	matrix.postRotate(0);
+	
+	// recreate the new Bitmap
+	Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, 
+						   width, height, matrix, true); 
+	return resizedBitmap;
+    }
 }
--- src/com/papp/PaddleApp.java
+++ src/com/papp/PaddleApp.java
@@ -8,17 +8,19 @@ import com.papp.R;
 import android.util.*;
 import android.widget.*;
 import android.view.View;
+import android.app.TabActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.*;
 import android.net.Uri;
+import android.content.res.*;
 import android.database.*;
 import android.provider.BaseColumns;
 import android.widget.AdapterView.OnItemClickListener;
 import android.view.*;
 import android.database.sqlite.*;
 
-public class PaddleApp extends Activity
+public class PaddleApp extends TabActivity
 {
 
   private void installDatabaseIfNeeded() {
@@ -33,14 +35,46 @@ public class PaddleApp extends Activity
   {
     super.onCreate(savedInstanceState);
     installDatabaseIfNeeded();
-    setContentView(R.layout.nextmatch);
     setTitle(R.string.version);
-    Intent intent = getIntent();
-    if (intent.getData() == null) {
-      intent.setData(Player.CONTENT_URI);
-    }
-    //    Player me = getMe();
-    final Context paddleAppCtx = this;
-    ContentResolver cr = getContentResolver();
+    setContentView(R.layout.main);
+
+    Resources res = getResources(); // Resource object to get Drawables
+    TabHost tabHost = getTabHost();  // The activity TabHost
+    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+    Intent intent;  // Reusable Intent for each tab
+
+    // Create an Intent to launch an Activity for the tab (to be reused)
+    intent = new Intent().setClass(this, NextMatch.class);
+
+    // Initialize a TabSpec for each tab and add it to the TabHost
+    spec = tabHost.newTabSpec("nextmatch").setIndicator("NextMatch",
+                      res.getDrawable(R.drawable.ic_tab_nextmatch))
+                  .setContent(intent);
+    tabHost.addTab(spec);
+
+    intent = new Intent().setClass(this, TeamStandings.class);
+    spec = tabHost.newTabSpec("teamstandings").setIndicator("TeamStandings",
+                      res.getDrawable(R.drawable.ic_tab_teamstandings))
+                  .setContent(intent);
+    tabHost.addTab(spec);
+
+    // Do the same for the other tabs
+    intent = new Intent().setClass(this, YourTeam.class);
+    spec = tabHost.newTabSpec("yourteam").setIndicator("YourTeam",
+                      res.getDrawable(R.drawable.ic_tab_yourteam))
+                  .setContent(intent);
+    tabHost.addTab(spec);
+
+
+    tabHost.setCurrentTab(1);
+
+    // setContentView(R.layout.nextmatch);
+    // Intent intent = getIntent();
+    // if (intent.getData() == null) {
+    //   intent.setData(Player.CONTENT_URI);
+    // }
+    // //    Player me = getMe();
+    // final Context paddleAppCtx = this;
+    // ContentResolver cr = getContentResolver();
   }
 }
