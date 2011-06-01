58e2bde9cc87a2a38f7f6e816452e4edc7f95db0
ubuntu on my asus <joshua.calloway@gmail.com>
2011-06-01 10:01:21 -0500

    added hyperlinks to phone numbers

--- src/com/papp/NextMatch.java
+++ src/com/papp/NextMatch.java
@@ -18,49 +18,69 @@ import android.content.*;
 import android.net.*;
 import android.util.Log;
 import android.widget.Gallery.LayoutParams;
-
+import android.text.method.LinkMovementMethod;
+import android.text.Spanned;
+import android.text.style.URLSpan;
+import android.text.style.StyleSpan;
+import android.text.SpannableString;
 
 public class NextMatch extends Activity {
-    private final static String TAG="NextMatch";
+  private final static String TAG="NextMatch";
 
-    /** Called when the activity is first created. */
-    @Override public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setTitle(R.string.version);
-	setContentView(R.layout.nextmatch);
-	ImageView clubPic = (ImageView)findViewById(R.id.club_imgview);
-	Log.v(TAG, "clubPic : " + clubPic);
-	Bitmap resized = resizeImage( R.drawable.valleylo_club);
-	BitmapDrawable bmd = new BitmapDrawable(resized);
-	clubPic.setImageDrawable(bmd);
-	clubPic.setScaleType(ScaleType.FIT_XY);
-    }
+  /** Called when the activity is first created. */
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTitle(R.string.version);
+    setContentView(R.layout.nextmatch);
+    ImageView clubPic = (ImageView)findViewById(R.id.club_imgview);
+    Log.v(TAG, "clubPic : " + clubPic);
+    Bitmap resized = resizeImage( R.drawable.valleylo_club);
+    BitmapDrawable bmd = new BitmapDrawable(resized);
+    clubPic.setImageDrawable(bmd);
+    clubPic.setScaleType(ScaleType.FIT_XY);
 
-    private Bitmap resizeImage(int drawableId){
+    SpannableString ss = new SpannableString("(312) 285-0344");
+    ss.setSpan(new StyleSpan(Typeface.ITALIC), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    ss.setSpan(new URLSpan("tel:3122850344"), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    TextView tv = (TextView)findViewById(R.id.captain_phonenumber);
+    tv.setText(ss);
+    tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+    ss = new SpannableString("(773) 111-9999");
+    ss.setSpan(new StyleSpan(Typeface.ITALIC), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    ss.setSpan(new URLSpan("tel:7731119999"), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    tv = (TextView)findViewById(R.id.other_captain_phonenumber);
+    tv.setText(ss);
+    tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+  }
+  
+  private Bitmap resizeImage(int drawableId){
 	
-	// load the origial BitMap (500 x 500 px)
-	Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), 
-							drawableId);
-	
-	int width = bitmapOrg.getWidth();
-	int height = bitmapOrg.getHeight();
-	int newWidth = 100;
-	int newHeight = 100;
-	
-	// calculate the scale - in this case = 0.4f
-	float scaleWidth = ((float) newWidth) / width;
-	float scaleHeight = ((float) newHeight) / height;
-	
-	// createa matrix for the manipulation
-	Matrix matrix = new Matrix();
-	// resize the bit map
-	matrix.postScale(scaleWidth, scaleHeight);
-	// rotate the Bitmap
-	matrix.postRotate(0);
-	
-	// recreate the new Bitmap
-	Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, 
-						   width, height, matrix, true); 
-	return resizedBitmap;
-    }
+    // load the origial BitMap (500 x 500 px)
+    Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), 
+						    drawableId);
+    
+    int width = bitmapOrg.getWidth();
+    int height = bitmapOrg.getHeight();
+    int newWidth = 100;
+    int newHeight = 100;
+    
+    // calculate the scale - in this case = 0.4f
+    float scaleWidth = ((float) newWidth) / width;
+    float scaleHeight = ((float) newHeight) / height;
+    
+    // createa matrix for the manipulation
+    Matrix matrix = new Matrix();
+    // resize the bit map
+    matrix.postScale(scaleWidth, scaleHeight);
+    // rotate the Bitmap
+    matrix.postRotate(0);
+    
+    // recreate the new Bitmap
+    Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0, 
+					       width, height, matrix, true); 
+    return resizedBitmap;
+  }
 }
